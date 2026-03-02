9dec4d872d8804883972e074979bdb56e8c54ab8
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2026-03-02 17:17:07 +0500

    Backend is implemented in root folder

--- app/pages/director/Dashboard.vue
+++ app/pages/director/Dashboard.vue
@@ -2,6 +2,10 @@
     <DirectorDashboard/>
 </template>
 <script setup lang="ts">
+const res = await $fetch('/api/hello')
+const res2 = await $fetch('/greating')
+console.log(res2)
+
 definePageMeta({
   layout: 'director'
 })
